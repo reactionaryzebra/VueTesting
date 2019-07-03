96f4c4a41dcd6df37eb3c2d3416c27c4efdda8c8
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-07-02 17:18:39 -0700

    add CompletedToDos component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <GetToDo></GetToDo>
     <CurrentToDos></CurrentToDos>
+    <CompletedToDos></CompletedToDos>
   </div>
 </template>
 
 <script>
 import GetToDo from "./components/GetToDo.vue";
 import CurrentToDos from "./components/CurrentToDos.vue";
+import CompletedToDos from "./components/CompletedToDos.vue";
 
 export default {
   name: "ToDo",
   components: {
     GetToDo,
-    CurrentToDos
+    CurrentToDos,
+    CompletedToDos
   }
 };
 </script>
